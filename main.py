d4d99c469120e90ff90875adc79b9808a1cbc08b
Tumpa-Prizrak <igrunok.play@yandex.ru>
2022-02-25 19:14:01 +0300

    0.0.1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Test")
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+class TestApp(App):
+    def build(self):
+        return Builder.load_file("Markup.np")
+
+TestApp().run()
